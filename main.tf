dae8318a138c49bb73edaf8b0e74741c1a651a0f
hnakashima2007 <129917884+hnakashima2007@users.noreply.github.com>
2023-08-04 11:48:25 +0900

    dev vpc

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "blog" {
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
-    Name = "Learning Terraform"
+    Name = "HelloWorld"
   }
 }
 
@@ -47,7 +47,7 @@ module "blog_sg" {
 
   name   = "blog"
   vpc_id = module.vpc.public_subnets[0]
-  
+
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
